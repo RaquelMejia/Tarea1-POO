c8f664209dc56c859ed58226e717b125520947e0
RaquelMejia <140339202+RaquelMejia@users.noreply.github.com>
2024-02-11 13:27:47 -0600

    Add files via upload

---
+++ TAREA LAB/TareaLaboratorio/Personaje.cs
@@ -0,0 +1,37 @@
+﻿indin using System;
+
+namespace AdministradorPersonajes
+    {
+        public enum TipoPersonaje
+        {
+            Guerrero,
+            Mago,
+            Sanador
+        }
+
+        public class Personaje
+        {
+            public string Nombre { get; set; }
+            public int Salud { get; set; }
+            public int Ataque { get; set; }
+            public int DefensaFisica { get; set; }
+            public int DefensaMagica { get; set; }
+            public int PoderMagico { get; set; }
+            public TipoPersonaje Tipo { get; set; }
+
+            public Personaje(string nombre, int salud, int ataque, int defensaFisica, int defensaMagica, int poderMagico, TipoPersonaje tipo)
+            {
+                Nombre = nombre;
+                Salud = salud;
+                Ataque = ataque;
+                DefensaFisica = defensaFisica;
+                DefensaMagica = defensaMagica;
+                PoderMagico = poderMagico;
+                Tipo = tipo;
+            }
+        }
+    }
+
+
+
+
